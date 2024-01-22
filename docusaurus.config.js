7ce32c3c5562e4dddde9af4e5d4b42db4ede8ea8
Thomas Dekeyser <thomas@polymerlabs.org>
2024-01-22 16:25:07 -0500

    update social card

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -62,7 +62,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
+      image: 'img/logo-svg-black.svg',
       navbar: {
         title: 'Developer Docs',
         hideOnScroll: false,
@@ -134,7 +134,7 @@ const config = {
             title: 'Community',
             items: [
               {
-                label: 'Developer Forupm',
+                label: 'Developer Forum',
                 href: 'https://forum.polymerlabs.org',
               },
               {
