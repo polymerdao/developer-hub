36a10b9ac1a518ece9131b43995869154060050f
tommyoc2 <148829819+tommyoc2@users.noreply.github.com>
2024-10-23 17:31:42 -0400

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,7 +18,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/quickstart/start">
+            to="/docs/build/start">
             Get Started Quickly ⏱️
           </Link>
         </div>
