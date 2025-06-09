80772d0ea75b661ee36e962775a6ad8c6ae5afbc
tommyoc2 <148829819+tommyoc2@users.noreply.github.com>
2025-06-09 10:44:20 -0400

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -61,7 +61,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/poly-socialcard.jpg',
+      image: 'img/Gradient + Transparent BG.png',
       navbar: {
         title: 'Docs',
         hideOnScroll: false,
